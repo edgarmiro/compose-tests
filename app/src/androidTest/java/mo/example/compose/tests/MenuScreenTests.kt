1c0e385830b331318b2e71cb9da70945f0d9d013
Edgar Miró Monleón <edgarmiro@gmail.com>
2024-10-25 12:20:51 +0200

    Optimize imports

--- app/src/androidTest/java/mo/example/compose/tests/MenuScreenTests.kt
+++ app/src/androidTest/java/mo/example/compose/tests/MenuScreenTests.kt
@@ -5,14 +5,10 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
-import org.junit.Rule
 
 @RunWith(AndroidJUnit4::class)
 class MenuScreenTests {
